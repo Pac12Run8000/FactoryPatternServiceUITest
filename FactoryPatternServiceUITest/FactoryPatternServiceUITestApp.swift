35f86bf498fc16464a791c3a7209b434c21d0189
Pac12Run8000 <gangstarr78992@>
2023-03-21 18:20:45 -0700

    Initial Commit

---
+++ FactoryPatternServiceUITest/FactoryPatternServiceUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryPatternServiceUITestApp.swift
+//  FactoryPatternServiceUITest
+//
+//  Created by Michelle Grover on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FactoryPatternServiceUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
